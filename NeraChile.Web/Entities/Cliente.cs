a86b7ebafdc530a1293db05a1f8205fea77649b5
Francisco Gonzalez <gonzalezuribe.francisco@gmail.com>
2020-07-20 14:40:09 -0400

    Cliente

---
+++ NeraChile.Web/Entities/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeraChile.Web.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido_Paterno { get; set; }
+        public string Apellido_Materno { get; set; }
+        public string Dirección { get; set; }
+        public string Comuna { get; set; }
+        public string  Ciudad { get; set; }
+        public string telefono { get; set; }
+
+    }
+}
